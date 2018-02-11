571e5dea1e3328be61537f1b31d0c23ce52c5fdd
Michael Neely <michaeljneelysd@gmail.com>
2018-02-11 12:06:22 +0000

    change data types of exam server interface

--- src/ie/nuigalway/ct414/assignment1/neelydaly/common/ExamServer.java
+++ src/ie/nuigalway/ct414/assignment1/neelydaly/common/ExamServer.java
@@ -5,23 +5,24 @@ package ie.nuigalway.ct414.assignment1.neelydaly.common;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import org.apache.commons.lang3.tuple.Pair;
 
 public interface ExamServer extends Remote {
 
 	// Return an access token that allows access to the server for some time period
-	public String login(int studentid, String password) throws 
+	public String login(String studentid, String password) throws 
 		UnauthorizedAccess, RemoteException;
 
-	// Return a summary list of Assessments currently available for this studentid
-	public List<String> getAvailableSummary(int token, int studentid) throws
+	// Return a summary list of Assessments currently available for this studentid (Pair - <AssessmentID, AssessmentInfo>)
+	public List<Pair<String,String>> getAvailableSummary(String token, String studentID) throws
 		UnauthorizedAccess, NoMatchingAssessment, RemoteException;
 
 	// Return an Assessment object associated with a particular course code
-	public Assessment getAssessment(int token, int studentid, String courseCode) throws
+	public Assessment getAssessmentById(String token, String studentID, String assessmentID) throws
 		UnauthorizedAccess, NoMatchingAssessment, RemoteException;
 
 	// Submit a completed assessment
-	public void submitAssessment(int token, int studentid, Assessment completed) throws 
+	public void submitAssessment(String token, String studentID, Assessment completed) throws 
 		UnauthorizedAccess, NoMatchingAssessment, RemoteException;
 
 }
