97994b51da42a733b8fb4b720fdb92b542df604e
Michael Neely <michaeljneelysd@gmail.com>
2018-02-12 17:10:45 +0000

    attempt

--- src/server/ExamEngine.java
+++ src/server/ExamEngine.java
@@ -16,6 +16,7 @@ public class ExamEngine implements ExamServer {
 	private StudentRegistry students;
 	private AssessmentRegistry assessments;
 	private CourseRegistry courses;
+	static Registry registry;
 
 	public ExamEngine(String logonDB, String studentDB, String assessmentDB, String courseDB) {
 		super();
@@ -81,10 +82,8 @@ public class ExamEngine implements ExamServer {
 			String courseDB = "../courses.txt";
 			String assessmentDB = "../assessments.txt";
 			ExamServer engine = new ExamEngine(logonDB, studentDB, assessmentDB, courseDB);
-			ExamServer stub =
-					(ExamServer) UnicastRemoteObject.exportObject(engine, 0);
-			Registry registry = LocateRegistry.getRegistry("127.0.0.1");
-			registry.rebind(name, stub);
+			registry = LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+			registry.rebind("ExamServer", engine);
 			System.out.println("ExamEngine bound");
 		} catch (Exception e) {
 			System.err.println("ExamEngine exception:");
