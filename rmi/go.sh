e6f50264b2a259bb1b154d10a4739870ead7f112
Michael Neely <michaeljneelysd@gmail.com>
2018-02-13 14:45:59 +0000

    use 80 for rmi

--- rmi/go.sh
+++ rmi/go.sh
@@ -1,2 +1,2 @@
 docker build -t my-rmi --build-arg SYSTEM_IP=159.65.23.223 .
-docker run -dit --net=host -p 159.65.23.223:1099:1099 my-rmi
+docker run -dit --net=host -p 159.65.23.223:1099:1099 -p 159.65.23.223:8080:80 my-rmi
