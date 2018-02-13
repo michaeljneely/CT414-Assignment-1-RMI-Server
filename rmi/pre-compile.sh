b645f7bca88a35d5fcacc60158c077b6a8e2d3d5
Michael Neely <michaeljneelysd@gmail.com>
2018-02-13 11:17:51 +0000

    aasd

--- rmi/pre-compile.sh
+++ rmi/pre-compile.sh
@@ -1,4 +1,4 @@
-curl https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar
-curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar
+curl https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar > junit.jar
+curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar > hamcrest.jar
 cp ../src/server/* ./
-javac -cp ../apache/ct414.jar:./junit-4.12.jar:./hamcrest-core-1.3.jar ./*.java
+javac -cp ../apache/ct414.jar:./junit.jar:./hamcrest.jar ./*.java
