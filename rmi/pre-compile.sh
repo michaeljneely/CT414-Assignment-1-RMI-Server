34544587c710e039b6c31772aa2dfa5639544442
Michael Neely <michaeljneelysd@gmail.com>
2018-02-13 11:31:52 +0000

    plz

--- rmi/pre-compile.sh
+++ rmi/pre-compile.sh
@@ -1,4 +1,5 @@
 curl https://search.maven.org/remotecontent?filepath=junit/junit/4.12/junit-4.12.jar > junit.jar
 curl https://search.maven.org/remotecontent?filepath=org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar > hamcrest.jar
-cp ../src/server/* ./
 javac -cp ../apache/ct414.jar:./junit.jar:./hamcrest.jar ./*.java
+docker build -t my-rmi .
+docker run -dit --name my-rmi --net=host -p 1099:1099 my-rmi
