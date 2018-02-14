4942ce598a146607b38b3fb30c8295630b31911f
Michael Neely <michaeljneelysd@gmail.com>
2018-02-14 11:49:55 +0000

    cleanup

--- rmi/go.sh
+++ rmi/go.sh
@@ -1,2 +1,2 @@
-docker build -t my-rmi --build-arg SYSTEM_IP=$1 .
-docker run -dit --net=host -p $1:1099:1099 my-rmi
+docker build -t rmiserver --build-arg SYSTEM_IP=$1 .
+docker run -dit --name my-rmi --net=host -p $1:1099:1099 rmiserver
